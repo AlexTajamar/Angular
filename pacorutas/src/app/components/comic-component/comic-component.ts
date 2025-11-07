b8568078a448302b0dfd19c1a8a569cb1b2bf48d
JuanTajamar <alejandro.ruizgarcia@tajamar365.com>
2025-11-07 10:17:36 +0100

    Before Services

---
+++ pacorutas/src/app/components/comic-component/comic-component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Comic } from '../../models/comic';
+
+@Component({
+  selector: 'app-comic',
+  standalone: false,
+  templateUrl: './comic-component.html',
+  styleUrl: './comic-component.css',
+})
+export class ComicComponent {
+  @Input() comic!: Comic;
+  @Input() index!: number;
+  @Output() seleccionarFavorito: EventEmitter<any> = new EventEmitter<any>();
+  @Output() deleteComic: EventEmitter<any> = new EventEmitter<any>();
+
+  marcarFavorito(): void {
+    this.seleccionarFavorito.emit(this.comic);
+  }
+
+  eliminarComic() {
+    this.deleteComic.emit(this.index);
+  }
+}
