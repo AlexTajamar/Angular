094df5c63ad76c511cefd0fdd380ab760e94916a
JuanTajamar <alejandro.ruizgarcia@tajamar365.com>
2025-11-07 12:57:45 +0100

    commit

--- serviciosangular/src/app/components/personas-standalone-true/personas-standalone-true.ts
+++ serviciosangular/src/app/components/personas-standalone-true/personas-standalone-true.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Persona } from '../../models/Persona';
+import { CommonModule } from '@angular/common';
+import { ServicioPersonas } from '../../services/service.persona';
 
 @Component({
   selector: 'app-personas-standalone-true',
   standalone: true,
   templateUrl: './personas-standalone-true.html',
   styleUrl: './personas-standalone-true.css',
+  imports: [CommonModule],
+  providers: [ServicioPersonas],
 })
-export class PersonasStandaloneTrue {}
+export class PersonasStandaloneTrue {
+  public personas!: Array<Persona>;
+  constructor() {
+    let p = new Persona(1, 'Angel', 'Terrorista Afgano', 69);
+    this.personas.push(p);
+    let p1 = new Persona(1, 'Angel', 'Terrorista de Lira', 69);
+    this.personas.push(p1);
+  }
+}
--- serviciosangular/src/app/services/service.persona.ts
+++ serviciosangular/src/app/services/service.persona.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { Persona } from '../models/Persona';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable()
 export class ServicioPersonas {
   //Un Observable<Any></Any> para devikver la peticion
   getPersonas(): Observable<any> {
-    let url = 'https://servicioapipersonasmvcpgs.azurewebsites.net/';
+    let url = environment.url;
     let request = 'api/personas';
     return this._httpClient.get(url + request);
   }
   getPersonasPromise(): Promise<any> {
-    let url = 'https://servicioapipersonasmvcpgs.azurewebsites.net/';
+    let url = environment.url;
     let request = 'api/personas';
     let promise = new Promise((resolve) => {
       this._httpClient.get(url + request).subscribe((response) => {
